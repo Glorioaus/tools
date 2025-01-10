be7131601ac3bc81bae0cd8e10a537303e97e6be
1050627 <jtmaz@sunnyoptical.com>
2025-01-10 11:11:59 +0800

    图片居中

--- app/components/ToolCardContent.tsx
+++ app/components/ToolCardContent.tsx
@@ -29,11 +29,13 @@ const ToolCardContent: React.FC<ToolCardContentProps> = ({
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className={`p-3 rounded-lg ${isHovered ? 'scale-110' : ''}`}>
+      <div className="flex items-center justify-center w-full h-full">
         <img
           src={`/path/${Icon}`}
           alt={`Icon for ${title}`}
           className="w-10 h-10 text-primary"
         />
+      </div>
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-gray-500">{description}</p>
       </div>
