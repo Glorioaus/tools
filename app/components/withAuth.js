f7a6a8d00a1151d1f8ffbc352f04eae9e23101da
1050627 <jtmaz@sunnyoptical.com>
2025-01-13 16:15:18 +0800

    SSO修改

--- app/components/withAuth.js
+++ app/components/withAuth.js
@@ -8,30 +8,65 @@ const withAuth = WrappedComponent => {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-      // 假设我们有一个函数 checkAuth 来检查用户是否登录
+      // 检查用户是否登录
       const checkAuth = async () => {
         const ticket = new URLSearchParams(window.location.search).get('ticket')
-        if (ticket) {
-          setLoading(false) // 如果 ticket 存在，不做处理，直接设置 loading 为 false
+        let authMap = JSON.parse(localStorage.getItem('authMap')) || {}
+        const storedTicket = authMap.ticket
+        const storedTicketTimestamp = authMap.ticketTimestamp
+        const currentTime = new Date().getTime()
+
+        console.log('Checking authentication:', {
+          ticket,
+          storedTicket,
+          storedTicketTimestamp,
+          currentTime
+        })
+
+        if (
+          storedTicket &&
+          currentTime - parseInt(storedTicketTimestamp) <= 12 * 60 * 60 * 1000
+        ) {
+          console.log('Stored ticket is valid:', storedTicket)
+          setLoading(false)
           return
+        } else {
+          if (
+            !storedTicket ||
+            currentTime - parseInt(storedTicketTimestamp) > 12 * 60 * 60 * 1000
+          ) {
+            if (ticket) {
+              authMap = {
+                ticket: ticket,
+                ticketTimestamp: currentTime.toString()
+              }
+              localStorage.setItem('authMap', JSON.stringify(authMap))
+              console.log('Updated authMap with new ticket:', authMap)
+              setLoading(false)
+              return
+            } else {
+              localStorage.removeItem('authMap')
+              console.log(
+                'Redirecting to login page due to invalid or missing ticket'
+              )
+              router.push(
+                `https://sso.sunnyoptical.cn/login?service=${encodeURIComponent(
+                  window.location.origin
+                )}`
+              )
+              return
+            }
+          }
         }
 
-        const isAuthenticated = !!localStorage.getItem('authToken')
-        if (!isAuthenticated) {
-          router.push(
-            `https://sso.sunnyoptical.cn/login?service=${encodeURIComponent(
-              window.location.origin
-            )}`
-          )
-        } else {
-          setLoading(false) // 用户已登录，设置 loading 为 false
-        }
+        console.log('No ticket found, but stored ticket is valid')
+        setLoading(false)
       }
 
       checkAuth()
     }, [router])
 
-    // 如果用户已登录，渲染传入的组件
+    // 如果用户未登录，返回 null，否则渲染传入的组件
     if (loading) return null
 
     return <WrappedComponent {...props} />
